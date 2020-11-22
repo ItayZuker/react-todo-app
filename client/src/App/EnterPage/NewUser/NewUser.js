f14b40c9aa7c122b749ed1f618a5323ea9c659b6
Itay Zuker <zuker2085@gmail.com>
2020-11-23 00:26:24 +0200

    new user notification

--- client/src/App/EnterPage/NewUser/NewUser.js
+++ client/src/App/EnterPage/NewUser/NewUser.js
@@ -2,46 +2,58 @@ import React, {useState, useContext, useEffect} from 'react';
 import { appContext } from '../../../AppContext';
 import './new-user.scss';
 
-
-
 export function NewUser(){
 
     const context = useContext(appContext);
 
-    const [active, setActive] = useState(false);
-    const [notification, setNotification] = useState(false)
+    const [notification, setNotification] = useState('');
+    const [renderNotification, setRenderNotification] = useState(false);
 
-    useEffect(() => {
-        context.allUsers < 5 ? setActive(true) : setActive(false);
-    }, [context.allUsers]);
+    function handleError() {
+        setRenderNotification(true)
+        setTimeout(() => {
+            setRenderNotification(false)
+            setNotification('')
+        }, 1500);
+    }
 
     return <div className={'new-user-container'}>
         <form
             onSubmit={(e) => {
                 e.preventDefault();
-                if(!active || e.target.user.value === '') {
+                if(e.target.user.value === '') {
                     e.target.user.value = '';
-                    setNotification(true)
-                    setTimeout(() => {
-                        setNotification(false)
-                    }, 2000);
-                    return
-                }
-                fetch('/users/api', {
-                    method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/json',
-                    },
-                    body: JSON.stringify({
-                        name: e.target.user.value,
-                    }),
-                }).then((res) => {
-                    console.log(res);
+                    setNotification('Enter name!');
+                    handleError();
+                } else if(context.usersArray.find(user => {
+                    if(user.name === e.target.user.value) return true})) {
                     e.target.user.value = '';
-                    context.setRenderUsers(true);
-                })
+                    setNotification('No doubles..');
+                    handleError();
+                } else if(context.usersArray.length >= 5) {
+                    e.target.user.value = '';
+                    setNotification('Max five users');
+                    handleError();
+                } else {
+                    fetch('/users/api', {
+                        method: 'POST',
+                        headers: {
+                            'Content-Type': 'application/json',
+                        },
+                        body: JSON.stringify({
+                            name: e.target.user.value,
+                        }),
+                    }).then((res) => {
+                        console.log(res);
+                        e.target.user.value = '';
+                        context.setRenderUsers(true);
+                    });
+                }              
         }}>
-            <input className={notification ? 'notification' : ''} name='user' placeholder={active ? "Enter name" : 'Max 5 users..'}></input>
+            <input
+                className={renderNotification ? 'notification' : ''}
+                name='user'
+                placeholder={renderNotification ? notification : 'Enter name'}></input>
             <input className='submit' type='submit' value='Add'></input>
         </form>
     </div>
--- client/src/App/TodosPage/Dropdown/DropdownMenu/DropdownMenu.js
+++ client/src/App/TodosPage/Dropdown/DropdownMenu/DropdownMenu.js
@@ -1,15 +1,24 @@
-import React, {Redirect} from 'react';
+import React, {useContext} from 'react';
+import {useHistory} from 'react-router-dom';
+import { appContext } from '../../../../AppContext';
 import './dropdown-menu.scss';
 
 export function DropdownMenu(props) {
+
+    const context = useContext(appContext);
+
+    const history = useHistory(); // ---> This hook is for navigation control
+
     return <div className={'dropdown-menu-container ' + (props.openState ? 'open' : '')}>
         <div
             className='menu-item-container'
             onClick={() => {
                 fetch(`/users/api/${props.userId}`, {
                     method: 'DELETE'
-                });
-                // return to main page ?????
+                }).then(() => {
+                    history.push('/'); // ---> Go to homepage
+                    context.setRenderUsers(true);
+                })
             }}
             >
             <p>Delete user</p>
