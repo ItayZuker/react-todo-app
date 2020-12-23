71af6bd6ddecdf8e910885eaf46412ced25e3669
Itay Zuker <zuker2085@gmail.com>
2020-12-23 16:41:31 +0200

    fix

--- server/index.js
+++ server/index.js
@@ -10,6 +10,7 @@ require('./db.js');
 app.set('json spaces', 2);
 app.use(express.json());
 
+
 app.use('/todos/api', todosRout);
 app.use('/lists/api', listsRout)
 app.use('/users/api', usersRout);
