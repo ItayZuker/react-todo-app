4220bdf1215d296e4471ed44e82e4ef31cc4af53
Itay Zuker <zuker2085@gmail.com>
2020-12-25 10:28:04 +0200

    refreshing fix

--- server/index.js
+++ server/index.js
@@ -18,7 +18,7 @@ app.use('/visitors/api', visitorsRout);
 app.use('/', express.static(path.join(__dirname, '../client/build')));
 
 
-app.get('/*', function(req, res) {
+app.use('/*', function(req, res) {
     res.sendFile(path.join(__dirname, '../client/build/index.html'), function(err) {
       if (err) {
         res.status(500).send(err)
