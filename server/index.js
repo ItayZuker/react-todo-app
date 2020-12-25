5af533b993b555dcb90bf7b5143039583a0efb44
Itay Zuker <zuker2085@gmail.com>
2020-12-25 14:07:26 +0200

    refreshing fix

--- server/index.js
+++ server/index.js
@@ -10,23 +10,13 @@ require('./db.js');
 app.set('json spaces', 2);
 app.use(express.json());
 
-// app.get('/*', express.static(path.join(__dirname, '../client/build')))
 
 app.use('/todos/api', todosRout);
 app.use('/lists/api', listsRout);
 app.use('/users/api', usersRout);
 app.use('/visitors/api', visitorsRout);
-// app.use('/', express.static(path.join(__dirname, '../client/build')))
+app.use('/*', express.static(path.join(__dirname, '../client/build')))
 
-// app.get('/*', express.static(path.join(__dirname, '../client/build')))
-
-app.use('/', function(req, res) {
-    res.sendFile(path.join(__dirname, '../client/build'), function(err) {
-      if (err) {
-        res.status(500).send(err)
-      }
-    })
-  })
 
 let port = 8080
 if(process.env.PORT) {
